799328136b523f308dc9a0b9e005c978b7802d48
Chetans2004 <chetanshinagari@gmail.com>
2025-12-24 22:27:20 +0530

    Day 1: Created Users table with constraints

--- Day1/first.sql
+++ Day1/first.sql
@@ -1,6 +1,6 @@
 
 use startsql;
-create table Users(
+create table student(
 	Users_id int auto_increment primary key,
     name varchar(50) not null,
     email varchar(50) unique not null,
@@ -8,4 +8,5 @@ create table Users(
     Join_date date
     );
     select * from Users;
+ 
  
